09c5e53c07067dbfd2d32162cdd8a33709ac10c2
NathanAWOA <141329199+NathanAWOA@users.noreply.github.com>
2023-09-18 18:57:08 -0300

    Estudos de node continuam

--- just starting/exercices/ex006.js
+++ just starting/exercices/ex006.js
@@ -3,18 +3,33 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res) {
-    res.send("Welcome")
+    res.sendfile(__dirname + "/html/index.html")
+
+    /*
+    PARECE QUE FUNCIONARIA SÓ COM O ./DIR/FILE
+    MAS TA BOM NÉ
+    */
 })
 
 app.get("/about", function(req, res) {
-    res.send("About")
+    res.sendfile(__dirname + "/html/about.html")
 })
 
 app.get("/blog", function(req, res) {
     res.send("Blog")
 })
 
+app.get("/hello/:cargo/:nome/:cor", function(req, res) {
+    res.send(
+        `<h1> Hello ${req.params.nome}</h1>
+        <h2> Seu cargo é ${req.params.cargo}</h2>
+        <h3> Sua cor favorita é ${req.params.cor}</h3>
+        `)
 
+    /*
+    O RES.SEND SÓ PODE SER ENVIANDO UMA UNICA VEZ, SE ENVIAR MAIS DE UM IRA DAR ERRO
+    */
+})
 
 
 app.listen(8081, function() {
